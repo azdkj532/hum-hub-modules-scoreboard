9fd0f9d4faa9e8a005092a2ec1f6b63d1deb95dd
Squirrel <kanade123321@gmail.com>
2015-12-14 07:24:22 +0800

    config module

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+use humhub\widgets\TopMenu;
+use humhub\modules\user\models\User;
+
+return [
+    'id' => 'scoreboard',
+    'class' => 'humhub/modules/scoreboard/Module',
+    'namespace' => 'humhub/modules/scoreboard',
+    'events' => [
+        ['class' => TopMenu::className(), 'event' => TopMenu::EVENT_INIT, 'callback' => ['humhub\modules\scoreboard\Events', 'onTopMenuInit']],
+    ]
+]
+
+?>
