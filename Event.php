73a37b106005b689ff1df6576c9c9714908e37c7
Squirrel <kanade123321@gmail.com>
2015-12-14 08:25:23 +0800

    create event handler

---
+++ Event.php
@@ -0,0 +1,19 @@
+<?php
+namespace humhub/modules/scoreboard;
+
+use Yii;
+
+class Events extends \yii\base\Object
+{
+    public static function onTopMenuInit($event)
+    {
+        $event->sender->addItem(array(
+            'label' => ("ScoreBoard.base", "ScoreBoard"),
+            'url' => Url::to(['/scoreboard'),
+            'icon' => '<i class="fa fa-bitcoin"></i>',
+            'isActive' => (Yii::$app->controller->module && Yii::$app->controller->module->id == 'scoreboard'),
+            'sortOrder' => 1000,
+        ));
+    }
+}
+?>
