804eb19def5408cf8160bfd64e0cea0e41381e17
Squirrel <kanade123321@gmail.com>
2015-12-14 09:04:00 +0800

    create basic view

---
+++ migrations/uninstall.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Migration;
+
+class uninstall extends Migration
+{
+
+    public function up()
+    {
+
+        $this->dropTable('scoreboard');
+    }
+
+    public function down()
+    {
+        echo "uninstall does not support migration down.\n";
+        return false;
+    }
+
+}
